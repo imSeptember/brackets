974107e9127a432c217115ac7a7ccb9078ac85ac
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2023-08-14 14:28:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   const stack = [];
+
+  for (let char of str) {
+    let isOpeningBracket = false;
+    let matchingBracket = null;
+
+    for (let pair of brackets) {
+      if (char === pair[0]) {
+        isOpeningBracket = true;
+        matchingBracket = pair[1];
+        break;
+      } else if (char === pair[1]) {
+        matchingBracket = pair[0];
+        break;
+      }
+    }
+
+    if (isOpeningBracket) {
+      stack.push(matchingBracket);
+    } else {
+      if (stack.length === 0 || stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
